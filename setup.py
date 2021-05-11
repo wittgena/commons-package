d7bcf7a0c3501c2b636171b5b6ffbf0b320bf84c
wittgena <wittgena@gmail.com>
2021-05-12 04:36:28 +0900

    FIX - update setup.py: add install_requires

--- setup.py
+++ setup.py
@@ -8,4 +8,12 @@ setup(
     author='Wittgena Lee',
     author_email='wittgena@gmail.com',
     packages=find_packages(),
+    install_requires=[
+        "<selenium>",
+        "<boto.s3>",
+        "<xmltodict>",
+        "google-api-python-client",
+        "oauth2client",
+        "openpyxl"
+    ]
 )
