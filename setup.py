ccd40c8cf7c2e9f0439018fab0ae6962be8e456f
wittgena <wittgena@gmail.com>
2021-05-12 09:34:50 +0900

    FIX - setup install_requires

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     author_email='wittgena@gmail.com',
     packages=find_packages(),
     install_requires=[
-        "<selenium>",
-        "<boto.s3>",
-        "<xmltodict>",
+        "selenium",
+        "boto.s3",
+        "xmltodict",
         "google-api-python-client",
         "oauth2client",
         "openpyxl"
