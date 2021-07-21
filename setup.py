71cea9af3e3dd3b1a4bf716a96aeb88f3139565e
wittgena <wittgena@gmail.com>
2021-07-22 06:03:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='commons',
-    version='1.0.2',
+    version='1.0.3',
     description='commons library',
     author='Wittgena Lee',
     author_email='wittgena@gmail.com',
